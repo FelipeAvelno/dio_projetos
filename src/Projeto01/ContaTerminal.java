b5341219ef883dc341271c0075022470da59d34c
Felipe Avelino <felipe.avelino10101@gmail.com>
2025-03-16 09:47:56 -0300

    feat: add tratamento de exceções

--- src/Projeto01/ContaTerminal.java
+++ src/Projeto01/ContaTerminal.java
@@ -1,41 +1,45 @@
 package Projeto01;
 
-import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.US);
 
-        Integer numero;
-        String agencia;
-        String nomeCliente;
-        Float saldo;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            scanner.useLocale(Locale.US);
 
-        System.out.println("Insira o número da conta: ");
-        numero = scanner.nextInt();
+            Integer numero;
+            String agencia;
+            String nomeCliente;
+            Float saldo;
 
-        System.out.println("Insira o número da agência");
-        agencia = scanner.next();
+            System.out.println("Insira o número da conta: ");
+            numero = scanner.nextInt();
 
-        System.out.println("Insira seu nome: ");
-        nomeCliente = scanner.next();
+            System.out.println("Insira o número da agência");
+            agencia = scanner.next();
 
-        System.out.println("Insira seu saldo: ");
-        saldo = scanner.nextFloat();
+            System.out.println("Insira seu nome: ");
+            nomeCliente = scanner.next();
 
-        if (agencia != null && nomeCliente != null && !nomeCliente.isBlank()) {
-            System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta número %d e seu saldo de %f já está disponível para saque", nomeCliente, agencia, numero, saldo);
-            scanner.close();
+            System.out.println("Insira seu saldo: ");
+            saldo = scanner.nextFloat();
+
+            if (agencia != null && nomeCliente != null && !nomeCliente.isBlank()) {
+                System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta número %d e seu saldo de %f já está disponível para saque", nomeCliente, agencia, numero, saldo);
+                scanner.close();
+            }
+
+            else {
+                System.out.println("Você deixou de inserir valores válidos");
+                scanner.close();
+            }
+        } catch (InputMismatchException e) {
+            System.out.printf("Erro %s", e);
         }
-
-        else {
-            System.out.println("Você deixou de inserir valores válidos");
-            scanner.close();
-        }
-
     }
-
 }
